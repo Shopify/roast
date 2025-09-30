254332ef07823e13e2033ddd427530e403261ba1
Sam Schmidt <sam.schmidt@shopify.com>
2025-09-30 15:13:28 -0400

    Set up block executor

---
+++ dsl/prototype.rb
@@ -0,0 +1,12 @@
+# typed: true
+# frozen_string_literal: true
+
+#: self as Roast::DSL::Executor
+
+config do
+  puts "Configuring stuff"
+end
+
+execute do
+  puts "Running stuff"
+end
--- lib/roast/dsl/executor.rb
+++ lib/roast/dsl/executor.rb
@@ -4,19 +4,46 @@
 module Roast
   module DSL
     class Executor
+      class ExecutorError < Roast::Error ; end
+      class ExecutorAlreadyPreparedError < ExecutorError ; end
+      class ExecutorAlreadyCompletedError < ExecutorError ; end
+
       class << self
         def from_file(workflow_path)
-          execute(File.read(workflow_path))
+          run!(File.read(workflow_path))
         end
 
         private
 
-        def execute(input)
-          new.instance_eval(input)
+        def run!(input)
+          executor = new
+          executor.prepare!(input)
+          executor.start!
         end
       end
 
-      # Define methods to be used in workflows below.
+      def prepare!(input)
+        # You can only initialize an executor once.
+        raise ExecutorAlreadyPreparedError if @prepared
+        instance_eval(input)
+        @prepared = true
+      end
+
+      def config
+        # Do any initial cog setup
+        yield
+      end
+
+      def execute
+        # Move the cogs into the roast state machine
+        yield
+      end
+
+      def start!
+        # Now we run the cogs!
+        # You can only do this once, executors are not reusable to avoid state pollution
+        raise ExecutorAlreadyCompletedError if @completed
+      end
 
       def shell(command_string)
         output, _status = Roast::Helpers::CmdRunner.capture2e(command_string)
