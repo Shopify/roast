e97294d8000e1ad6595f8c8943b0f1b8c838cedd
Nathan FG <38052787+nfgrep@users.noreply.github.com>
2025-10-01 10:02:06 -0400

    Drop in "cog" DSL system (#416)
    * Drop cog system in
    
    * try execute blocks
    
    * Revert "try execute blocks"
    
    This reverts commit 8f03092c56557fc1a04bb94a95e4ab0c8d6523e5.

---
+++ lib/roast/dsl/cog/store.rb
@@ -0,0 +1,27 @@
+# typed: true
+# frozen_string_literal: true
+
+module Roast
+  module DSL
+    class Cog
+      class Store
+        class << self
+          #: (Symbol) -> Roast::DSL::Cog?
+          def find(id)
+            store[id]
+          end
+
+          #: (Symbol, Roast::DSL::Cog) -> Roast::DSL::Cog
+          def insert(id, inst)
+            store[id] = inst
+          end
+
+          #: () -> Hash[Symbol, Roast::DSL::Cog]
+          def store
+            @store ||= {}
+          end
+        end
+      end
+    end
+  end
+end
