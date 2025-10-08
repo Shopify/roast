369a32b45a0c1743c7dee9097413e931d6bcad04
juniper-shopify <201364921+juniper-shopify@users.noreply.github.com>
2025-10-08 11:27:13 -0400

    Add types to cmd cog

--- lib/roast/dsl/cogs/cmd.rb
+++ lib/roast/dsl/cogs/cmd.rb
@@ -6,10 +6,21 @@ module Roast
     module Cogs
       class Cmd < Cog
         class Output
+          #: String?
           attr_reader :output
+
+          #: String?
           attr_reader :err
+
+          #: Process::Status?
           attr_reader :status
 
+
+          #: (
+          #|  String? output,
+          #|  String? error,
+          #|  Process::Status? status
+          #| ) -> void
           def initialize(output, error, status)
             @output = output
             @err = error
@@ -18,10 +29,12 @@ module Roast
         end
 
         class Config < Cog::Config
+          #: () -> void
           def print_all!
             @values[:print_all] = true
           end
 
+          #: () -> bool
           def print_all?
             !!@values[:print_all]
           end
@@ -29,6 +42,7 @@ module Roast
 
         use_config_class Config
 
+        #: () -> void
         def execute
           result = Output.new(*Roast::Helpers::CmdRunner.capture3(input))
           puts result.output if @config.print_all?
