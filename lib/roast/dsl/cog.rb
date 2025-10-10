f64bd2b7fd68ad7ac05e56fadab949595601c8ad
juniper-shopify <201364921+juniper-shopify@users.noreply.github.com>
2025-10-10 00:30:37 -0400

    Add type annotations to cog.rb

--- lib/roast/dsl/cog.rb
+++ lib/roast/dsl/cog.rb
@@ -4,17 +4,21 @@
 module Roast
   module DSL
     class Cog
-      class CogAlreadyRanError < StandardError; end
+      class CogError < Roast::Error; end
+      class CogAlreadyRanError < CogError; end
 
       class << self
+
+        #: () -> void
         def on_create
           eigen = self
-          proc do |instance_name = Random.uuid, &action|
+          proc do |instance_name = Random.uuid, &cog_input_proc|
             #: self as Roast::DSL::ExecutionContext
-            add_cog_instance(instance_name, eigen.new(instance_name, action))
+            add_cog_instance(instance_name, eigen.new(instance_name, cog_input_proc))
           end
         end
 
+        #: () -> void
         def on_config
           eigen = self
           proc do |cog_name = nil, &configuration_proc|
@@ -29,26 +33,38 @@ module Roast
           end
         end
 
+        #: () -> singleton(Cog::Config)
         def config_class
           @config_class ||= find_child_config_or_default
         end
 
         private
 
+        #: () -> singleton(Cog::Config)
         def find_child_config_or_default
           config_constant = "#{name}::Config"
           const_defined?(config_constant) ? const_get(config_constant) : Cog::Config # rubocop:disable Sorbet/ConstantsFromStrings
         end
       end
 
-      attr_reader :name, :output
+      #: Symbol
+      attr_reader :name
 
+      #: untyped
+      attr_reader :output
+
+      #: (Symbol, Proc) -> void
       def initialize(name, cog_input_proc)
         @name = name
-        @cog_input_proc = cog_input_proc
-        @finished = false
+        @cog_input_proc = cog_input_proc #: Proc
+        @output = nil #: untyped
+        @finished = false #: bool
+
+        # Make sure a config is always defined, so we don't have to worry about nils
+        @config = self.class.config_class.new #: Cog::Config
       end
 
+      #: (Cog::Config, CogInputContext) -> void
       def run!(config, input_context)
         raise CogAlreadyRanError if ran?
 
@@ -57,11 +73,13 @@ module Roast
         @finished = true
       end
 
+      #: () -> bool
       def ran?
         @finished
       end
 
       # Inheriting cog must implement this
+      #: (untyped) -> untyped
       def execute(input)
         raise NotImplementedError
       end
