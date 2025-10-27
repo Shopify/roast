91540bcb80991e0ec0d3cc4b9d748cc147fcc0b0
juniper-shopify <201364921+juniper-shopify@users.noreply.github.com>
2025-10-27 10:18:30 -0400

    Add bindings for cog? methods to CogInputContext

--- lib/roast/dsl/cog_input_manager.rb
+++ lib/roast/dsl/cog_input_manager.rb
@@ -6,9 +6,13 @@ module Roast
     # Context in which an individual cog block within the `execute` block of a workflow is evaluated
     class CogInputManager
       class CogOutputAccessError < Roast::Error; end
+
       class CogDoesNotExistError < CogOutputAccessError; end
+
       class CogNotYetRunError < CogOutputAccessError; end
+
       class CogSkippedError < CogOutputAccessError; end
+
       class CogFailedError < CogOutputAccessError; end
 
       #: (Cog::Registry, Cog::Store) -> void
@@ -31,11 +35,14 @@ module Roast
 
       #: (Symbol) -> void
       def bind_cog(cog_method_name)
+        cog_question_method_name = (cog_method_name.to_s + "?").to_sym
         cog_bang_method_name = (cog_method_name.to_s + "!").to_sym
         cog_output_method = method(:cog_output)
+        cog_output_question_method = method(:cog_output?)
         cog_output_bang_method = method(:cog_output!)
         @context.instance_eval do
           define_singleton_method(cog_method_name, proc { |cog_name| cog_output_method.call(cog_name) })
+          define_singleton_method(cog_question_method_name, proc { |cog_name| cog_output_question_method.call(cog_name) })
           define_singleton_method(cog_bang_method_name, proc { |cog_name| cog_output_bang_method.call(cog_name) })
         end
       end
@@ -50,6 +57,11 @@ module Roast
         nil
       end
 
+      #: (Symbol) -> bool
+      def cog_output?(cog_name)
+        !cog_output(cog_name).nil?
+      end
+
       #: (Symbol) -> Cog::Output
       def cog_output!(cog_name)
         raise CogDoesNotExistError, cog_name unless @cogs.key?(cog_name)
@@ -59,11 +71,6 @@ module Roast
           raise CogSkippedError, cog_name if cog.skipped?
           raise CogFailedError, cog_name if cog.failed?
         end.output
-      end
-
-      #: (Symbol) -> bool
-      def cog_output?(cog_name)
-        !cog_output(cog_name).nil?
       end
     end
   end
