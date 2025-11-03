381977bb7cf376527cd6190c2fd9859d0a37187a
Juniper <201364921+juniper-shopify@users.noreply.github.com>
2025-11-03 11:40:27 -0500

    Add method doc to cogs config (#514)
    Part of a gradual process to add comprehensive documentation comments to all
    user-facing interfaces before the v1.0 release

--- lib/roast/dsl/cog/config.rb
+++ lib/roast/dsl/cog/config.rb
@@ -29,20 +29,65 @@ module Roast
           self.class.new(values.merge(config_object.values))
         end
 
-        # It is recommended to implement a custom config object for a nicer interface,
-        # but for simple cases where it would just be a key value store we provide one by default.
-
+        # Set a configuration value using hash-style syntax
+        #
+        # This method provides basic key-value storage for cog configuration.
+        # All standard Roast cogs use imperative setter methods for config values.
+        # It is recommended that custom cogs implement their own config classes with similar methods
+        # for a more structured interface, but this hash-style syntax is provided for simple cases.
+        #
+        # #### See Also
+        # - `[]`
+        #
         #: (Symbol, untyped) -> void
         def []=(key, value)
           @values[key] = value
         end
 
+        # Get a configuration value using hash-style syntax
+        #
+        # This method provides basic key-value retrieval for cog configuration.
+        # All standard Roast cogs use imperative setter methods for config values.
+        # It is recommended that custom cogs implement their own config classes with similar methods
+        # for a more structured interface, but this hash-style syntax is provided for simple cases.
+        #
+        # #### See Also
+        # - `[]=`
+        #
         #: (Symbol) -> untyped
         def [](key)
           @values[key]
         end
 
         class << self
+          # Define a configuration field with simple, out-of-the-box getter/setter behavior
+          # and default value handling
+          #
+          # #### Generated Methods
+          # This method creates two methods for a configuration field:
+          # 1. A dual-purpose method (`key`) that gets the value when called without arguments,
+          #    or sets the value when called with an argument.
+          # 2. A bang method (`use_default_#{key}!`) that explicitly resets the field to its default value.
+          #
+          # When getting a value without arguments, the configured value is returned if set,
+          # otherwise the default value is returned.
+          # When setting a value with an argument, the validator block is applied if provided.
+          #
+          # #### Validation
+          #
+          # This method accepts an optional `validator` block that will be called with the new value
+          # when the field's setter method is invoked. The validator should raise an exception if the
+          # provided value is not valid. It's return value will be used as the new config value.
+          # This allows the validator to coerce an value into a standard form if desired.
+          #
+          # ##### See Also
+          # - `Cog::Config#validate!` - validates the config object as a whole, after all values have been set
+          #
+          # #### Parameters
+          # - `key` - The name of the configuration field
+          # - `default` - The default value for this field
+          # - `validator` - Optional block that validates and/or transforms the value before storing it
+          #
           #: [T] (Symbol, T) ?{(T) -> T} -> void
           def field(key, default, &validator)
             default = default #: as untyped
@@ -65,37 +110,124 @@ module Roast
           end
         end
 
-        #: () -> void
-        def exit_on_error!
-          @values[:exit_on_error] = true
-        end
-
-        #: () -> void
-        def no_exit_on_error!
-          @values[:exit_on_error] = false
-        end
-
-        #: () -> bool
-        def exit_on_error?
-          @values[:exit_on_error] ||= true
-        end
-
+        # Configure the cog to run asynchronously in the background
+        #
+        # When configured to run asynchronously, the cog will execute in the background
+        # and the next cog in the workflow will be able to start immediately without waiting
+        # for this cog to complete.
+        #
+        # If this cog has started running, attempts to access its output from another cog will
+        # block until this cog completes.
+        # If this cog has not yet started, attempts to access its output from another cog will
+        # fail in the same way that accessing the output of a synchronous cog that has not yet
+        # run would fail.
+        #
+        # The workflow will not complete until all asynchronous cogs have completed (or failed).
+        #
+        # #### Inverse Methods
+        # - `no_async!`
+        # - `sync!`
+        #
+        # #### See Also
+        # - `async?`
+        #
         #: () -> void
         def async!
           @values[:async] = true
         end
 
+        # Configure the cog __not__ to run asynchronously
+        #
+        # When configured not to run asynchronously, the cog will execute synchronously
+        # and the next cog in the workflow will wait for this cog to complete before starting.
+        #
+        # #### Alias Methods
+        # - `no_async!`
+        # - `sync!`
+        #
+        # #### Inverse Methods
+        # - `async!`
+        #
+        # #### See Also
+        # - `async?`
+        #
         #: () -> void
         def no_async!
           @values[:async] = false
         end
 
+        # Check if the cog is configured to run asynchronously
+        #
+        # #### See Also
+        # - `async!`
+        # - `no_async!`
+        # - `sync!`
+        #
         #: () -> bool
         def async?
           !!@values[:async]
         end
 
-        alias_method(:continue_on_error!, :no_exit_on_error!)
+        # Configure the cog to abort the workflow immediately if it fails to complete successfully
+        #
+        # Enabled by default.
+        #
+        # #### Alias Methods
+        # - `abort_on_error!`
+        # - `exit_on_error!`
+        #
+        # #### Inverse Methods
+        # - `continue_on_error!`
+        # - `no_abort_on_error!`
+        # - `no_exit_on_error!`
+        #
+        # #### See Also
+        # - `abort_on_error?`
+        #
+        #: () -> void
+        def abort_on_error!
+          @values[:exit_on_error] = true
+        end
+
+        # Configure the cog __not__ to abort the workflow if it fails to complete successfully
+        #
+        # When a cog is configured not to abort on error, the workflow will continue to run subsequent cogs
+        # even if that cog fails. However, attempts to access that cog's output from another cog will fail.
+        #
+        # #### Alias Methods
+        # - `continue_on_error!`
+        # - `no_abort_on_error!`
+        # - `no_exit_on_error!`
+        #
+        # #### Inverse Methods
+        # - `abort_on_error!`
+        # - `exit_on_error!`
+        #
+        # #### See Also
+        # - `abort_on_error?`
+        #
+        #: () -> void
+        def no_abort_on_error!
+          @values[:exit_on_error] = false
+        end
+
+        # Check if the cog is configured to abort the workflow immediately on error
+        #
+        # #### See Also
+        # - `abort_on_error!`
+        # - `continue_on_error!`
+        # - `exit_on_error!`
+        # - `no_abort_on_error!`
+        # - `no_exit_on_error!`
+        #
+        #: () -> bool
+        def abort_on_error?
+          @values[:exit_on_error] ||= true
+        end
+
+        alias_method(:exit_on_error!, :abort_on_error!)
+        alias_method(:no_exit_on_error!, :no_abort_on_error!)
+        alias_method(:continue_on_error!, :no_abort_on_error!)
         alias_method(:sync!, :no_async!)
       end
     end
