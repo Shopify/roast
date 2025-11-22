0f03cfba31bb4040f9d171beb6c3e0a007cfa84c
juniper-shopify <201364921+juniper-shopify@users.noreply.github.com>
2025-11-21 23:15:59 -0500

    Replace print_* config methods with show_* config methods for `cmd` cog

--- lib/roast/dsl/cogs/cmd.rb
+++ lib/roast/dsl/cogs/cmd.rb
@@ -5,63 +5,38 @@ module Roast
   module DSL
     module Cogs
       class Cmd < Cog
-        # TODO: User-facing doc comment about the cog's configuration options would ideally go here,
-        #   and then get copied to config_context.rbi by the tapioca compiler.
-        #   For now, just writing the doc comments there to avoid duplication
+        # Configure the `cmd` cog
+        #
+        # See sorbet/rbi/shims/lib/roast/dsl/config_context.rbi for full class documentation.
         class Config < Cog::Config
-          # Configure the cog to write both STDOUT and STDERR to the console
-          #
-          # - Alias: `print_all!`
-          # - Alias: `display!`
-          #
-          #: () -> void
-          def print_all!
-            # comment about how this method works
-            @values[:print_stdout] = true
-            @values[:print_stderr] = true
-          end
-
-          # Configure the cog to write __no output__ to the console, neither STDOUT nor STDERR
-          #
-          # - Alias: `no_print_all!`
-          # - Alias: `no_display!`
-          #
-          #: () -> void
-          def print_none!
-            @values[:print_stdout] = false
-            @values[:print_stderr] = false
-          end
-
           # Configure the cog to write STDOUT to the console
           #
-          # Disabled by default
+          # Disabled by default.
+          #
+          # #### See Also
+          # - `no_show_stdout!`
+          # - `show_stdout?`
+          # - `display!`
           #
           #: () -> void
-          def print_stdout!
-            @values[:print_stdout] = true
+          def show_stdout!
+            raise "⚠️ DEPRECATION: use #{__callee__.to_s.sub("print_", "show_")} instead of #{__callee__}" if __callee__.to_s.include?("print_")
+
+            @values[:show_stdout] = true
           end
 
           # Configure the cog __not__ to write STDOUT to the console
           #
+          # #### See Also
+          # - `show_stdout!`
+          # - `show_stdout?`
+          # - `no_display!`
+          #
           #: () -> void
-          def no_print_stdout!
-            @values[:print_stdout] = false
-          end
+          def no_show_stdout!
+            raise "⚠️ DEPRECATION: use #{__callee__.to_s.sub("print_", "show_")} instead of #{__callee__}" if __callee__.to_s.include?("print_")
 
-          # Configure the cog to write STDERR to the console
-          #
-          # Disabled by default
-          #
-          #: () -> void
-          def print_stderr!
-            @values[:print_stderr] = true
-          end
-
-          # Configure the cog __not__ to write STDERR to the console
-          #
-          #: () -> void
-          def no_print_stderr!
-            @values[:print_stderr] = false
+            @values[:show_stdout] = false
           end
 
           # Configure the cog to strip surrounding whitespace from the values in its output object
@@ -84,18 +59,57 @@ module Roast
 
           # Check if the cog is configured to write STDOUT to the console
           #
+          # #### See Also
+          # - `show_stdout!`
+          # - `no_show_stdout!`
+          #
           #: () -> bool
-          def print_stdout?
-            !!@values[:print_stdout]
+          def show_stdout?
+            !!@values[:show_stdout]
+          end
+
+          # Configure the cog to write STDERR to the console
+          #
+          # Disabled by default.
+          #
+          # #### See Also
+          # - `no_show_stderr!`
+          # - `show_stderr?`
+          # - `display!`
+          #
+          #: () -> void
+          def show_stderr!
+            raise "⚠️ DEPRECATION: use #{__callee__.to_s.sub("print_", "show_")} instead of #{__callee__}" if __callee__.to_s.include?("print_")
+
+            @values[:show_stderr] = true
+          end
+
+          # Configure the cog __not__ to write STDERR to the console
+          #
+          # #### See Also
+          # - `show_stderr!`
+          # - `show_stderr?`
+          # - `no_display!`
+          #
+          #: () -> void
+          def no_show_stderr!
+            raise "⚠️ DEPRECATION: use #{__callee__.to_s.sub("print_", "show_")} instead of #{__callee__}" if __callee__.to_s.include?("print_")
+
+            @values[:show_stderr] = false
           end
 
           # Check if the cog is configured to write STDERR to the console
           #
+          # #### See Also
+          # - `show_stderr!`
+          # - `no_show_stderr!`
+          #
           #: () -> bool
-          def print_stderr?
-            !!@values[:print_stderr]
+          def show_stderr?
+            !!@values[:show_stderr]
           end
 
+<<<<<<< HEAD
           # Check if the cog is configured to write its output to the console in raw form
           #
           #: () -> bool
@@ -105,6 +119,67 @@ module Roast
 
           alias_method(:display!, :print_all!)
           alias_method(:no_display!, :print_none!)
+=======
+          # Configure the cog to write both STDOUT and STDERR to the console
+          #
+          # #### Alias Methods
+          # - `display!`
+          # - `print_all!`
+          #
+          # #### See Also
+          # - `no_display!`
+          # - `show_stdout!`
+          # - `show_stderr!`
+          #
+          #: () -> void
+          def display!
+            raise "⚠️ DEPRECATION: use display! instead of #{__callee__}" if __callee__.to_s.include?("print_")
+
+            @values[:show_stdout] = true
+            @values[:show_stderr] = true
+          end
+
+          # Configure the cog to write __no output__ to the console, neither STDOUT nor STDERR
+          #
+          # #### Alias Methods
+          # - `no_display!`
+          # - `print_none!`
+          # - `quiet!`
+          #
+          # #### See Also
+          # - `display!`
+          # - `no_show_stdout!`
+          # - `no_show_stderr!`
+          #
+          #: () -> void
+          def no_display!
+            raise "⚠️ DEPRECATION: use no_display! instead of #{__callee__}" if __callee__.to_s.include?("print_")
+
+            @values[:show_stdout] = false
+            @values[:show_stderr] = false
+          end
+
+          # Check if the cog is configured to display any output while running
+          #
+          # #### See Also
+          # - `display!`
+          # - `no_display!`
+          # - `show_stdout?`
+          # - `show_stderr?`
+          #
+          #: () -> bool
+          def display?
+            show_stdout? || show_stderr?
+          end
+
+          alias_method(:quiet!, :no_display!)
+          alias_method(:print_all!, :display!)
+          alias_method(:print_none!, :no_display!)
+          alias_method(:print_stdout!, :show_stdout!)
+          alias_method(:no_print_stdout!, :no_show_stdout!)
+          alias_method(:print_stderr!, :show_stderr!)
+          alias_method(:no_print_stderr!, :no_show_stderr!)
+>>>>>>> c623a68 (Replace print_* config methods with show_* config methods for `cmd` cog)
         end
 
         class Input < Cog::Input
@@ -169,8 +244,8 @@ module Roast
         def execute(input)
           config = @config #: as Config
 
-          stdout_handler = config.print_stdout? ? ->(line) { $stdout.print(line) } : nil
-          stderr_handler = config.print_stderr? ? ->(line) { $stderr.print(line) } : nil
+          stdout_handler = config.show_stdout? ? ->(line) { $stdout.print(line) } : nil
+          stderr_handler = config.show_stderr? ? ->(line) { $stderr.print(line) } : nil
 
           stdout, stderr, status = CommandRunner #: as untyped
             .execute(
