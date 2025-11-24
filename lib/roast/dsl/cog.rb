15034d08455e0e75a250b5df8221886ada24534f
Juniper <201364921+juniper-shopify@users.noreply.github.com>
2025-11-24 16:17:23 -0500

    Clean up asynchronous cog handling (#575)
    Simplifies and makes more explicit some of the flow control handling for async cogs

--- lib/roast/dsl/cog.rb
+++ lib/roast/dsl/cog.rb
@@ -6,8 +6,6 @@ module Roast
     class Cog
       class CogError < Roast::Error; end
 
-      class CogAlreadyRanError < CogError; end
-
       class CogAlreadyStartedError < CogError; end
 
       class << self
@@ -51,8 +49,6 @@ module Roast
       def initialize(name, cog_input_proc)
         @name = name
         @cog_input_proc = cog_input_proc #: ^(Cog::Input) -> untyped
-        @run_semaphore = Async::Semaphore.new(1) #: Async::Semaphore
-        @started = false #: bool
         @output = nil #: Cog::Output?
         @skipped = false #: bool
         @failed = false #: bool
@@ -63,11 +59,9 @@ module Roast
 
       #: (Cog::Config, CogInputContext, untyped, Integer) -> Async::Task
       def run!(config, input_context, executor_scope_value, executor_scope_index)
-        @run_semaphore.async do # prevents multiple invocations of run! from occurring in parallel
-          raise CogAlreadyRanError if @output.present? || @skipped || @failed
-          raise CogAlreadyStartedError if @started
+        raise CogAlreadyStartedError if @task
 
-          @started = true
+        @task = Async() do
           @config = config
           input_instance = self.class.input_class.new
           input_return = input_context.instance_exec(
@@ -76,8 +70,10 @@ module Roast
           coerce_and_validate_input!(input_instance, input_return)
           @output = execute(input_instance)
         rescue ControlFlow::SkipCog
+          # TODO: do something with the message passed to skip!
           @skipped = true
         rescue ControlFlow::FailCog => e
+          # TODO: do something with the message passed to fail!
           @failed = true
           # TODO: better / cleaner handling in the workflow execution manager for a workflow failure
           #   just re-raising this exception for now
@@ -85,59 +81,37 @@ module Roast
         end
       end
 
-      #: () -> bool
-      def started?
-        # NOTE: explicitly not waiting for the cog's task to complete before answering, as in the other methods below,
-        # because this method is intended to be potentially called while the cog is running.
-        @started
+      #: () -> void
+      def wait
+        @task&.wait
       end
 
       #: () -> bool
-      def ran?
-        # NOTE: this will block until the cog finished running IF it is currently running
-        # It will answer immediately if the cog has not started to run
-        answer = @run_semaphore.async do
-          @output.present? || @skipped || @failed
-        end.wait
-        # NOTE: this answer will be `nil` if this block that is simply checking for the output/result is stopped,
-        # not if the cog's task itself was stopped. This should really never happen.
-        raise CogError, "task stopped while fetching value" if answer.nil?
-
-        answer
+      def started?
+        @task.present?
       end
 
       #: () -> bool
       def skipped?
-        # see NOTEs on `ran?`
-        answer = @run_semaphore.async do
-          @skipped
-        end.wait
-        raise CogError, "task stopped while fetching value" if answer.nil?
-
-        answer
+        @skipped
       end
 
       #: () -> bool
       def failed?
-        # see NOTEs on `ran?`
-        answer = @run_semaphore.async do
-          @failed
-        end.wait
-        raise CogError, "task stopped while fetching value" if answer.nil?
-
-        answer
+        @failed || !!@task&.failed?
       end
 
       #: () -> bool
       def stopped?
-        # see NOTEs on `ran?`
-        answer = @run_semaphore.async do
-          @started && !@output.present? && !@skipped && !@failed
-        end.wait
-        raise CogError, "task stopped while fetching value" if answer.nil?
-
-        answer
+        !!@task&.stopped?
       end
+
+      #: () -> bool
+      def succeeded?
+        @output.present? && @task&.finished?
+      end
+
+      protected
 
       # Inheriting cog must implement this
       #: (Cog::Input) -> Cog::Output
