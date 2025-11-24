847d02a9a6a54ed2b40666f35d5d3db24bee6b9f
Juniper <201364921+juniper-shopify@users.noreply.github.com>
2025-11-24 16:01:24 -0500

    Add .text and .lines methods to cog outputs that have textual content (#569)
    These are basically just convenience methods that made it easy to get
    the textual response of a cog with surrounding whitespace and newlines stripped,
    and to get an array of lines with each line similarly stripped.
    
    When a cog's output contains elements you want to process one at a time
    (which happens often when a `cmd` cog produces a list of something, files, etc.),
    it's common to have to split the text into lines and strip off the trailing newlines.
    This makes it easier.

--- dsl/step_communication.rb
+++ dsl/step_communication.rb
@@ -14,9 +14,8 @@ execute do
   cmd(:ls) { "ls -al" }
   cmd(:echo) do |my|
     my.command = "echo"
-    # TODO: this is a bespoke output object for cmd, is there a generic one we can offer
-    first_line = cmd!(:ls).out.split("\n").second
-    last_line = cmd!(:ls).out.split("\n").last
+    first_line = cmd!(:ls).lines.second
+    last_line = cmd!(:ls).lines.last
     my.args << first_line unless first_line.blank?
     my.args << "\n---\n"
     my.args << last_line if last_line != first_line && last_line.present?
