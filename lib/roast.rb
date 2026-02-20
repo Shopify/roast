4405ed6b767e1b80f6bc387ec424624396dd38cc
Juniper Alanna <201364921+juniper-shopify@users.noreply.github.com>
2026-02-20 12:02:44 -0500

    Remove Thor and simplify CLI args

--- lib/roast.rb
+++ lib/roast.rb
@@ -10,6 +10,7 @@ require "json"
 require "logger"
 require "net/http"
 require "open3"
+require "optparse"
 require "pathname"
 require "securerandom"
 require "shellwords"
@@ -32,7 +33,6 @@ require "active_support/notifications"
 require "async"
 require "async/semaphore"
 require "ruby_llm"
-require "thor"
 
 # Require project components that will not get automatically loaded
 require "roast/nil_assertions"
@@ -46,74 +46,117 @@ loader.ignore("#{__dir__}/roast-ai.rb")
 loader.setup
 
 module Roast
-  ROOT = File.expand_path("../..", __FILE__)
-
-  class CLI < Thor
-    desc "execute [WORKFLOW_CONFIGURATION_FILE] [FILES...]", "Run a workflow"
-    option :verbose, type: :boolean, aliases: "-v", desc: "Show output from all steps as they are executed"
-    option :executor, type: :string, default: "dsl", desc: "Set workflow executor (DEPRECATED: 'dsl' is the only valid option)"
-
-    def execute(*paths)
-      raise Thor::Error, "Workflow file is required" if paths.empty?
-      raise StandardError, "'dsl' is the only valid executor" if options[:executor] != "dsl"
-
-      workflow_path, *files = paths
-
-      targets, workflow_args, workflow_kwargs = parse_custom_workflow_args(files, ARGV)
-      targets.unshift(options[:target]) if options[:target]
-      workflow_params = Roast::WorkflowParams.new(targets, workflow_args, workflow_kwargs)
-
-      # If the workflow is running with a working directory specified to be different from the current directory
-      # from which Roast was run, and the workflow file if specified with a relative path, check for it relative
-      # to the current directory first, and then relative to the working directory specified for the workflow.
-      roast_working_directory = Pathname.new(File.expand_path(ENV["ROAST_WORKING_DIRECTORY"] || Dir.pwd))
-      workflow_path = Pathname.new(workflow_path)
-      real_workflow_path = if workflow_path.absolute? || workflow_path.exist?
-        workflow_path
-      else
-        roast_working_directory / workflow_path
-      end.realpath
-
-      Dir.chdir(roast_working_directory) do
-        Roast::Workflow.from_file(real_workflow_path, workflow_params)
-      end
-    rescue => e
-      if options[:verbose]
-        raise e
-      else
-        Roast::Log.error(e.message)
-      end
-    end
-
-    desc "version", "Display the current version of Roast"
-
-    def version
-      puts "Roast version #{Roast::VERSION}"
-    end
-
-    private
-
-    #: (Array[String], Array[String]) -> [Array[String], Array[Symbol], Hash[Symbol, String]]
-    def parse_custom_workflow_args(parsed_args, raw_args)
-      separator_index = raw_args.index("--")
-      extra_args = (separator_index ? raw_args[(separator_index + 1)..] : []) || []
-      targets = parsed_args.shift(parsed_args.length - extra_args.length)
-      args = []
-      kwargs = {}
-      parsed_args.each do |arg|
-        if arg.include?("=")
-          key, value = arg.split("=", 2)
-          kwargs[key.to_sym] = value if key
-        else
-          args << arg.to_sym
-        end
-      end
-      [targets, args, kwargs]
-    end
+  class CLI
+    COMMANDS = ["execute", "version", "help"].freeze
 
     class << self
-      def exit_on_failure?
-        true
+      def start(argv)
+        roast_args, extra_args = split_at_separator(argv)
+
+        show_help = false #: bool
+        parser = OptionParser.new do |opts|
+          opts.on("-h", "--help") { show_help = true }
+        end
+        parser.order!(roast_args)
+
+        command = roast_args.first
+        if show_help || command == "help"
+          help
+        elsif command == "version"
+          puts "Roast version #{Roast::VERSION}"
+        elsif command == "execute"
+          roast_args.shift
+          run_execute(roast_args, extra_args)
+        elsif command && resolve_workflow_path(command)
+          run_execute(roast_args, extra_args)
+        elsif command
+          $stderr.puts "Could not find command or workflow file \"#{command}\"\n\n"
+          help
+          exit(1)
+        else
+          help
+        end
+      end
+
+      private
+
+      def run_execute(args, extra_args)
+        if args.empty?
+          $stderr.puts "Error: Workflow file is required\n\n"
+          help
+          exit(1)
+        end
+
+        workflow_path, *targets = args
+        real_workflow_path = resolve_workflow_path(workflow_path)
+        unless real_workflow_path
+          $stderr.puts "Error: Workflow file not found: #{workflow_path}\n\n"
+          help
+          exit(1)
+        end
+
+        workflow_args, workflow_kwargs = parse_custom_workflow_args(extra_args)
+        workflow_params = Roast::WorkflowParams.new(targets, workflow_args, workflow_kwargs)
+        roast_working_directory = Pathname.new(File.expand_path(ENV["ROAST_WORKING_DIRECTORY"] || Dir.pwd))
+        Dir.chdir(roast_working_directory) do
+          Roast::Workflow.from_file(real_workflow_path, workflow_params)
+        end
+      end
+
+      # Resolve a workflow path string to a real filesystem path.
+      # Returns the resolved Pathname, or nil if the file cannot be found.
+      #: (String) -> Pathname?
+      def resolve_workflow_path(workflow_path)
+        roast_working_directory = Pathname.new(File.expand_path(ENV["ROAST_WORKING_DIRECTORY"] || Dir.pwd))
+        path = Pathname.new(workflow_path)
+        resolved = if path.absolute? || path.exist?
+          path
+        else
+          roast_working_directory / path
+        end
+        resolved.realpath
+      rescue Errno::ENOENT
+        nil
+      end
+
+      #: (Array[String]) -> [Array[Symbol], Hash[Symbol, String]]
+      def parse_custom_workflow_args(extra_args)
+        args = []
+        kwargs = {}
+        extra_args.each do |arg|
+          arg = arg.sub(/^--?(?=[^-])/, "")
+          if arg.include?("=")
+            key, value = arg.split("=", 2)
+            kwargs[key.to_sym] = value if key
+          else
+            args << arg.to_sym
+          end
+        end
+        [args, kwargs]
+      end
+
+      def help
+        $stderr.puts <<~HELP
+          Usage: roast <workflow_file> [options] [targets...] [-- workflow_args]
+                 roast execute <workflow_file> [options] [targets...] [-- workflow_args]
+
+          Commands:
+            execute  Run a workflow (optional; any unrecognized command is treated as a workflow file to execute)
+            version  Display the current version of Roast
+            help     Show this help message
+
+          Options:
+            -h, --help     Show this help message
+        HELP
+      end
+
+      def split_at_separator(argv)
+        index = argv.index("--")
+        if index
+          [argv[0...index], argv[(index + 1)..]]
+        else
+          [argv.dup, []]
+        end
       end
     end
   end
